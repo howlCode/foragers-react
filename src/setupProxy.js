160030da44a1f431f79a09a589c98ab45ca435f8
howlCode <miromanyth@gmail.com>
2018-11-15 14:46:12 -0500

    deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,8 +2,10 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    proxy("/auth/google", { target: "http://localhost:5000/" }),
-    proxy("/api/*", { target: "http://localhost:5000/" }),
-    proxy("/api/stripe/course", { target: "http://localhost:5000/" })
+    proxy("/auth/google", { target: "https://foragers-api.herokuapp.com/" }),
+    proxy("/api/*", { target: "https://foragers-api.herokuapp.com/" }),
+    proxy("/api/stripe/course", {
+      target: "https://foragers-api.herokuapp.com/"
+    })
   );
 };
